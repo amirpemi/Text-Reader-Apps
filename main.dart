f36a63ac696c6a3f6c1ca044c7d481b5d2143708
amirpemi <45591385+amirpemi@users.noreply.github.com>
2021-01-10 16:17:32 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,46 @@
+import 'package:tts_1/widget/text_recognition_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String title = 'EasyReader';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.teal),
+        home: MainPage(title: title),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    @required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              const SizedBox(height: 25),
+              TextRecognitionWidget(),
+              const SizedBox(height: 15),
+            ],
+          ),
+        ),
+      );
+}
